6e31b3c65a4b7199ea36da536bfdc2677240c68d
dongxu <i@huangdx.net>
2016-04-08 16:08:23 +0800

    frontend: show process by host

--- frontend/app/scripts/controllers/hostStatusController.js
+++ frontend/app/scripts/controllers/hostStatusController.js
@@ -9,7 +9,18 @@
 angular.module('tiAdminApp')
     .controller('HostStatusCtrl', ['$stateParams', '$scope', '$http', '$timeout', function($stateParams, $scope, $http, $timeout) {
         $scope.machID = $stateParams.machID;
-        $http.get("http://localhost:8080/api/v1/hosts/" + $scope.machID).then(function(resp) {
-            $scope.data = resp.data;
-        });
+
+        var refresh = function() {
+            $http.get("http://localhost:8080/api/v1/hosts/" + $scope.machID).then(function(resp) {
+                $scope.data = resp.data;
+                console.log(resp.data);
+            });
+
+            $http.get("http://localhost:8080/api/v1/processes/findByHost?machID=" + $scope.machID).then(function(resp) {
+                $scope.processes = resp.data;
+                console.log(resp.data);
+            });
+        }
+        refresh();
+
     }]);
